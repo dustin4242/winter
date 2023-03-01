66ccb72d236593b871869ff3a0c41bf207613354
dustin4242 <thegameexposer@gmail.com>
2023-03-01 09:25:58 -0600

    Implemented first wolf hunt for unknown variables being used

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,5 @@
-use std::fs::read_to_string;
-
 use crate::tokenizer::{tokenizer, Token};
+use std::{collections::HashMap, fs::read_to_string};
 
 pub fn parser(mut tokens: Vec<Token>) -> String {
     let mut asm_file = [
@@ -8,22 +7,27 @@ pub fn parser(mut tokens: Vec<Token>) -> String {
         vec!["segment readable writeable".to_string()],
         vec!["segment readable executable\nentry $".to_string()],
     ];
-
+    let mut variables: HashMap<String, String> = HashMap::new();
     let mut pos = 0;
     while pos < tokens.len() {
         match tokens[pos].value_type.as_str() {
             "word" => {
-                if tokens[pos + 1].value_type == "paren_open".to_string() {
-                    let func_name = &tokens[pos].value;
-                    let func_arguments = &tokens[pos + 2].value;
-                    asm_file[2].push(format!("{func_name}({func_arguments})"));
-                }
+                if variables.get(&tokens[pos].value).is_some() {
+                    if tokens[pos + 1].value_type == "paren_open".to_string() {
+                        let func_name = &tokens[pos].value;
+                        let func_arguments = &tokens[pos + 2].value;
+                        asm_file[2].push(format!("{func_name}({func_arguments})"));
+                    }
+                } else {
+                    panic!("Wolf found unknown variable: {}", tokens[pos].value);
+                };
             }
             "keyword" => match tokens[pos].value.as_str() {
                 "let" => {
                     let name = &tokens[pos + 1].value;
                     let value = &tokens[pos + 2].value;
                     let value_size = value.len();
+                    variables.insert(name.to_string(), tokens[pos + 2].value_type.clone());
                     asm_file[1].push(format!("{name} db {value_size},\"{value}\""));
                     pos += 2
                 }
@@ -32,6 +36,7 @@ pub fn parser(mut tokens: Vec<Token>) -> String {
                     let append_tokens = tokenizer(
                         read_to_string(format!("./snowfiles/{}.snw", tokens[pos].value)).unwrap(),
                     );
+                    variables.insert(tokens[pos].value.clone(), "function".to_string());
                     tokens.remove(pos);
                     for i in 0..append_tokens.len() {
                         let value = &append_tokens[i];
@@ -50,12 +55,7 @@ pub fn parser(mut tokens: Vec<Token>) -> String {
         }
         pos += 1;
     }
-    asm_file[2].push(
-        "mov rax, 60
-mov rdi, 0
-syscall"
-            .to_string(),
-    );
+    asm_file[2].push("mov rax, 60\nmov rdi, 0\nsyscall".to_string());
     let final_file =
         asm_file[0].join("\n") + "\n" + &asm_file[1].join("\n") + "\n" + &asm_file[2].join("\n");
     return final_file;
