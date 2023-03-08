4abea57eb518bfe729e23e205c6ebde5434b6941
dustin4242 <thegameexposer@gmail.com>
2023-03-08 17:08:43 -0600

    Made a optimized compilerun script

--- compilerunoptimized.sh
+++ compilerunoptimized.sh
@@ -2,7 +2,7 @@ if [ $1 ] && [ -r $1 ]
 then
 filename=${1/.snw/}
 ./target/release/winter "$filename.snw" &&
-rustc "$filename.rs" -o "$filename" -C opt-level=0 > /dev/null &&
+rustc "$filename.rs" -o "$filename" -C opt-level=3 > /dev/null &&
 rm -rf "$filename.rs" &&
 chmod +x $filename &&
 ./$filename
