8990bb413f5dcda5b65edfb09b5cbf7d53485925
dustin4242 <thegameexposer@gmail.com>
2023-03-08 17:03:27 -0600

    updated vim file and compilerun scripts

--- compilerun.sh
+++ compilerun.sh
@@ -2,7 +2,7 @@ if [ $1 ] && [ -r $1 ]
 then
 filename=${1/.snw/}
 ./target/release/winter "$filename.snw" &&
-rustc "$filename.rs" > /dev/null &&
+rustc "$filename.rs" -o "$filename" > /dev/null &&
 rm -rf "$filename.rs" &&
 chmod +x $filename &&
 ./$filename
