ddc36d066493a952ef56ade7d3024ba18dcf1e24
dustin4242 <thegameexposer@gmail.com>
2023-03-08 18:23:31 -0600

    and changed binary

--- snowfiles/main.rs
+++
@@ -1,8 +0,0 @@
-#[allow(unused, dead_code)]
-fn main() {
-fn print(msg: String) { print!("{msg}") }
-fn print_num(num: i8) { print!("{}", num) }
-let mut num: i8 = 5;
-print_num(num)
-let mut hello: String = "World".to_string();
-print(hello)}
