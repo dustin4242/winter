c859e3823e129595ece67a669f2b8af7f48347dd
dustin4242 <thegameexposer@gmail.com>
2023-03-13 12:45:26 -0500

    Made the compilerun script a bit more verbose

--- compilerun.sh
+++ compilerun.sh
@@ -1,10 +1,17 @@
 if [ $1 ] && [ -r $1 ]
 then
 filename=${1/.snw/}
+echo "Building Compiler..."
+cargo build --release > /dev/null &&
+echo "Compiling..." &&
+start=`date +%s%N | cut -b1-13`
 ./target/release/winter "$filename.snw" &&
 rustc "$filename.rs" -o "$filename" -C opt-level=0 > /dev/null &&
+end=`date +%s%N | cut -b1-13`
+echo "Finished in $((end-start)) milliseconds"
 rm -rf "$filename.rs" &&
 chmod +x $filename &&
+echo "Output:"
 ./$filename
 else echo "Didn't Specify A Winter File To Compile"
 fi
