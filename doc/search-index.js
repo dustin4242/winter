a646534b149a641ed081460a8f2e6dd568ba5976
dustin4242 <thegameexposer@gmail.com>
2023-06-09 09:47:03 +0000

    Also got rid of unnessary stuff

--- doc/search-index.js
+++
@@ -1,5 +0,0 @@
-var searchIndex = JSON.parse('{\
-"winter":{"doc":"Maximum Documentation :]","t":[],"n":[],"q":[],"d":[],"i":[],"f":[],"p":[]}\
-}');
-if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
-if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
--- doc/source-files.js
+++
@@ -1,4 +0,0 @@
-var sourcesIndex = JSON.parse('{\
-"winter":["",[],["artemis.rs","hail.rs","interpreter.rs","keywords.rs","main.rs","parser.rs","tokenizer.rs"]]\
-}');
-createSourceSidebar();
