4f3eb576707dd0ebeba3825b24e6ff058b0999af
dustin4242 <thegameexposer@gmail.com>
2023-07-22 07:45:10 -0500

    using next instead of nth

--- src/compile_needed/parser/handlers/operator.rs
+++ src/compile_needed/parser/handlers/operator.rs
@@ -4,7 +4,7 @@ pub fn handler(tokens: &mut Vec<Snowflake>, pos: usize, final_file: &mut Vec<Str
     let current = tokens[pos].value.to_owned();
     let mut next = &mut tokens[pos + 1];
     if current == "+" && next.value_type == Types::String {
-        next.value = next.value.split(".to_owned()").nth(0).unwrap().to_owned();
+        next.value = next.value.split(".to_owned()").next().unwrap().to_owned();
     }
     final_file.push(tokens[pos].value.to_owned());
     pos
